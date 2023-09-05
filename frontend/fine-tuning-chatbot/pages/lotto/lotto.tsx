d9ad427f110eb2797805c1aaa2c0fa3184fb4bb8
“Donghyeok <qwer5383@naver.com>
2023-09-05 09:54:58 +0900

    .

--- frontend/fine-tuning-chatbot/pages/lotto/lotto.tsx
+++ frontend/fine-tuning-chatbot/pages/lotto/lotto.tsx
@@ -5,6 +5,12 @@ const lotto: React.FC = () => {
 
 	const lotto_numbers: number[] = Array.from({ length: 45 }, (_, index) => index + 1);
 
+	const chooseNumber = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+		// 여기서 클릭된 div 요소와 관련된 작업을 수행할 수 있습니다.
+		debugger;
+	}
+	  
+
 	return (
 		<div>
 		<h1>테이블 만들기</h1>
@@ -14,7 +20,7 @@ const lotto: React.FC = () => {
 				<td width={"300px"}>
 					{/* <div style={{whiteSpace: 'pre-wrap'}}> */}
 					{lotto_numbers.map((number) => (
-						<div style={{width: "30px", height:"45px", display: "inline-block", textAlign:"center", border:"1px solid #f56d6d", margin:"5px"}}>{number}</div>
+						<div onClick={chooseNumber} style={{width: "30px", height:"45px", display: "inline-block", textAlign:"center", border:"1px solid #f56d6d", margin:"5px"}}>{number}</div>
 					))}
 					{/* </div> */}
 				
