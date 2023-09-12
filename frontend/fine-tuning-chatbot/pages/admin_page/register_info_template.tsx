fa285a0ecdf749d89f34ef813851abe08e9fe9f6
Ddongh <qwer5383@naver.com>
2023-09-12 23:03:07 +0900

    면접준비

--- frontend/fine-tuning-chatbot/pages/admin_page/register_info_template.tsx
+++ frontend/fine-tuning-chatbot/pages/admin_page/register_info_template.tsx
@@ -7,10 +7,11 @@ const Info_page: React.FC = () => {
     const [html, setHtml] = useState<string>(""); // HTML코드
     const [tagName, setTagName] = useState<string>("") // 수집할 태그 종류
     const [info, setInfo] = useState({});
-    const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+    // const [parsedHTML_0, setParsedHTML_0] = useState<HTMLElement|null>(null);
+    // const textareaRef = useRef<HTMLTextAreaElement | null>(null);
     const parsedHTML = useRef<HTMLDivElement | null>(null);
-    const [parsedHTML_0, setParsedHTML_0] = useState<HTMLDivElement | null>(null);
     
+
     const [activeTab, setActiveTab] = useState<string>('html');
     // const [html, setHtml] = useState<string>('');
     const [file, setFile] = useState('');
@@ -27,23 +28,22 @@ const Info_page: React.FC = () => {
         setInfo(updatedInfo);
     };
 
-    useEffect(() => {
-        // HTML 파싱 로직
-        if (parsedHTML.current) {
-            const parser = new DOMParser();
-            // const doc = parser.parseFromString(html.replaceAll("\n", "<br>"), 'text/html');
-            const doc = parser.parseFromString(html, 'text/html');
-            setParsedHTML_0(doc.documentElement);
-            // 파싱된 HTML을 parsedHTML 요소에 설정
-            // parsedHTML.current.innerHTML = '';
-            // parsedHTML.current.appendChild(doc.documentElement);
-
-            // setParsedHTML_0(doc.documentDivElement);
-        }
-    }, [html])
+    // useEffect(() => {
+    //     // HTML 파싱 로직
+    //     if (parsedHTML.current) {
+    //         const parser = new DOMParser();
+    //         // const doc = parser.parseFromString(html.replaceAll("\n", "<br>"), 'text/html');
+    //         const doc = parser.parseFromString(html, 'text/html');
+    //         setParsedHTML_0(doc.documentElement);
+    //         // 파싱된 HTML을 parsedHTML 요소에 설정
+    //         // parsedHTML.current.innerHTML = '';
+    //         // parsedHTML.current.appendChild(doc.documentElement);
+    //     }
+    // }, [html])
     
 
     const onHtmlChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        debugger;
         const newHtmlValue: string = e.target.value;
         setHtml(newHtmlValue); 
     };
@@ -61,6 +61,7 @@ const Info_page: React.FC = () => {
     }
 
     const test = () => {
+
         if(parsedHTML.current) {
             const tags = parsedHTML.current.querySelectorAll("input, select")
             
@@ -80,12 +81,13 @@ const Info_page: React.FC = () => {
                 }
               });
         } 
-        
-        
-    
     }
+    const buttonEvent = () => {
+        alert("33333")
+        }
     return (
         <div>
+            <button onClick={buttonEvent}> 1111</button>
             {/* <h1>1. html입력 및 결과 확인</h1> */}
             <div style={{display:'flex', justifyContent: 'center'}}>
                 <div style={{width:"500px", marginRight:"30px"}}>
@@ -132,34 +134,25 @@ const Info_page: React.FC = () => {
                             ></Upload>
                         </div>
                     </div>
-                    {/* <textarea 
-                    style={{height:"500px", resize: 'none'}} 
-                    ref={textareaRef} 
-                    value={html} 
-                    onChange={onHtmlChange} 
-                    placeholder="이곳에 HTML을 입력하세요."
-                    />     */}
                 </div>
                 <div style={{width:"500px", marginLeft:"30px"}}>
-                    {/* <h1 style={{textAlign:"center"}}>Parsed</h1>
-                    <Button type="primary">Primary Button</Button> */}
                     <div style={{ display: 'flex', alignItems: 'center' }}>
                         <h1 style={{ flex: 1, textAlign:"center"}}>Parsed</h1>
                         <Button type="primary" onClick={test}>Test</Button>
                     </div>
-                    <div ref={parsedHTML} style={{border:"1px solid black", height:"500px", overflowY:"auto"}}>
-                        <div dangerouslySetInnerHTML={{ __html: html }} />
-                    </div>
+                    <div dangerouslySetInnerHTML={{ __html: html }}  
+                    ref={parsedHTML} 
+                    style={{border:"1px solid black", height:"500px", overflowY:"auto", borderRadius:"10px" }} 
+                    />
+
                 </div>
             </div>
-            {/* <h1>2. 수집할 태그 종류 및 name입력 및 테스트</h1> */}
             <div style={{display:'flex', justifyContent: 'center', height:"500px"}}>
                 <div style={{width:"500px", height:"300px", marginRight:"30px"}}>
                     <h1 style={{textAlign:"center"}}>Error</h1>
                     <div style={{border:"1px solid black", minHeight:"300px"}}>
 
                     </div>
-
                 </div>
                 <div style={{width:"500px", height:"300px", marginLeft:"30px"}}>
                     <h1 style={{textAlign:"center"}}>Result</h1>
@@ -180,10 +173,7 @@ const Info_page: React.FC = () => {
                             ))}
                             </tbody>
                         </table>
-                        
-                    
                     </div>
-
                 </div>
             </div>
         </div>
